05e661878ead12d8b907044568bfe3b28630de5e
unknown <regime_skylark.0y@icloud.com>
2025-07-12 20:30:32 -0400

    Added unicode replacements

--- base/src/main.rs
+++ base/src/main.rs
@@ -61,7 +61,25 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let recipe = recipe.scale_to_servings(DEFAULT_SERVINGS, &converter);
 
-    let template = generate_recipe_html(&recipe, &converter);
+    let template = generate_recipe_html(&recipe, &converter)
+        .replace("1/2", "½")
+        .replace("1/3", "⅓")
+        .replace("2/3", "⅔")
+        .replace("1/4", "¼")
+        .replace("3/4", "¾")
+        .replace("1/5", "⅕")
+        .replace("2/5", "⅖")
+        .replace("3/5", "⅗")
+        .replace("4/5", "⅘")
+        .replace("1/6", "⅙")
+        .replace("5/6", "⅚")
+        .replace("1/7", "⅐")
+        .replace("1/8", "⅛")
+        .replace("3/8", "⅜")
+        .replace("5/8", "⅝")
+        .replace("7/8", "⅞")
+        .replace("1/9", "⅑")
+        .replace("1/10", "⅒");
 
     println!("{}", template);
 
